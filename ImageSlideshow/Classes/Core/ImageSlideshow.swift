7ae5f9047117fdd1ec498789e8b2fd6823afda64
antwater <antoni@tiket.com>
2019-05-21 11:16:51 +0700

    scrollView.contentInsetAdjustmentBehavior disable

--- ImageSlideshow/Classes/Core/ImageSlideshow.swift
+++ ImageSlideshow/Classes/Core/ImageSlideshow.swift
@@ -242,9 +242,9 @@ open class ImageSlideshow: UIView {
         scrollView.showsHorizontalScrollIndicator = false
         scrollView.showsVerticalScrollIndicator = false
         scrollView.autoresizingMask = autoresizingMask
-        if #available(iOS 11.0, *) {
-            scrollView.contentInsetAdjustmentBehavior = .never
-        }
+//        if #available(iOS 11.0, *) {
+//            scrollView.contentInsetAdjustmentBehavior = .never
+//        }
         addSubview(scrollView)
 
         if let pageIndicator = pageIndicator {
